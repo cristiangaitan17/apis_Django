24ed3d9ef7c0c7dd0e6ae2f881b53434c2f62cff
Cristian Steven Gaitan Hernandez <cristiangaitan1717@icloud.com>
2026-06-02 15:54:09 -0500

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-from decouple import config
-
-# LEER EL PUERTO DESDE EL ARCHIVO .ENV
-port = config('API_PORT', default='8000')
-
-# INICIAR EL SERVIDOR DE DESARROLLO DE DJANGO EN EL PUERTO LEIDO
-os.system(f'python manage.py runserver {port}')
