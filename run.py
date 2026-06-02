98132f4c646c6732d57c0a73d0a0df9faface545
Cristian Steven Gaitan Hernandez <cristiangaitan1717@icloud.com>
2026-06-02 15:40:56 -0500

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-from decouple import config
-
-# leer el puerto del archivo .env
-PORT = config('API_PORT')
-
-# ejecutar el servidor django
-os.system(f'python manage.py runserver {PORT}')
